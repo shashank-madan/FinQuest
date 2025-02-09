d0745823dc454172b3b1c77a7a121f8ee438fed2
shashank-madan <shashank.um.xl@gmail.com>
2025-02-08 23:28:36 -0500

    toasts fixed

--- components/games/credit-score-game.tsx
+++ components/games/credit-score-game.tsx
@@ -135,7 +135,7 @@ export function CreditScoreGame() {
         <p className="text-lg text-center text-purple-100">Score: {score}</p>
       </CardContent>
       <CardFooter className="justify-between">
-        <Button asChild variant="ghost">
+        <Button asChild>
           <Link href="/">Return to Realm Map</Link>
         </Button>
         {gameOver && <Button onClick={resetGame}>Play Again</Button>}
